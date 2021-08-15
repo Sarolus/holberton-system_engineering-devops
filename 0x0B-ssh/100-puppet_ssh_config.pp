1cba4ffb251cb39aa090f250ed4ac1e77adcd727
Sarolus <cactussius.primus@gmail.com>
2021-08-15 17:31:32 +0200

    Fix \t

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Add configuration lines in the client SSH config file.
-file_line {'Add identify file':
+file_line {'Add identity file':
     ensure => present,
     path   => '/etc/ssh/ssh_config',
-    line   => '/tIdentityFile ~/.ssh/holberton'
+    line   => '\tIdentityFile ~/.ssh/holberton'
 }
 
 file_line {'Set password auth off':
     ensure => present,
     path   => '/etc/ssh/ssh_config',
-    line   => '/tPasswordAuthentication no',
+    line   => '\tPasswordAuthentication no',
 }
