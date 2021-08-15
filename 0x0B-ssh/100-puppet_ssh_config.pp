86c96d21ef658647bd2c6be485665e90dedc91ee
Sarolus <cactussius.primus@gmail.com>
2021-08-15 17:28:37 +0200

    Add ssh project

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Add configuration lines in the client SSH config file.
+file_line {'Add identify file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '/tIdentityFile ~/.ssh/holberton'
+}
+
+file_line {'Set password auth off':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '/tPasswordAuthentication no',
+}
