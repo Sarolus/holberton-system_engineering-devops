808c583e5f88e38320a9ce4723e63e2ac80840bc
Sarolus <cactussius.primus@gmail.com>
2021-08-23 17:56:55 +0200

    Add task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# Configure a custom nginx response header
+
+package { 'nginx':
+  ensure => installed
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen \[::\]:80 default_server;',
+  line   => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
