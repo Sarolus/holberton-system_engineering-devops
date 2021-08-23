0992767bb8fe6c60493fff8e334cbfe0e852a38b
Sarolus <cactussius.primus@gmail.com>
2021-08-23 19:08:21 +0200

    Try different hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,10 +11,11 @@ package { 'nginx':
 }
 
 file_line { 'add_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen \[::\]:80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+  ensure  => present,
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
 }
 
 service { 'nginx':
