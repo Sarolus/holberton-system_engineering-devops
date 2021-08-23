621b7c9b69ec3e96d25a6659a6fd18d5359f2245
Sarolus <cactussius.primus@gmail.com>
2021-08-23 17:58:06 +0200

    Try different regex

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,7 +7,7 @@ package { 'nginx':
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen \[::\]:80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => 'add_header X-Served-By $hostname;',
 }
 
