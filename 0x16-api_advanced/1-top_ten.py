ba3335c29dc18830ea518b966dd453ef0f1567db
Sarolus <cactussius.primus@gmail.com>
2021-09-28 16:52:51 +0200

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Queries the Reddit API
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        Prints the titles of the first 10 hot posts for
+        a given subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    UserAgent = {"User-Agent": "ubuntu:test:v1.0 (by /u/kemitche)"}
+
+    request = requests.get(url, headers=UserAgent, allow_redirects=False)
+
+    if request.status_code == 404:
+        return 0
+
+    for post in request.json().get("data").get("children"):
+        print(post.get("data").get("title"))
