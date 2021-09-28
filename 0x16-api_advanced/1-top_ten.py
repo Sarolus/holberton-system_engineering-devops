d18527907e186a94cc9c6b5a792dff8e1eed4d97
Sarolus <cactussius.primus@gmail.com>
2021-09-28 16:55:53 +0200

    Fix invalid subreddit condition

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,7 +17,7 @@ def top_ten(subreddit):
     request = requests.get(url, headers=UserAgent, allow_redirects=False)
 
     if request.status_code == 404:
-        return 0
+        return "None"
 
     for post in request.json().get("data").get("children"):
         print(post.get("data").get("title"))
