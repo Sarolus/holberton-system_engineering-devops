2e818d84bf3d4245727550ec950e7278e64d8b65
Sarolus <cactussius.primus@gmail.com>
2021-09-28 16:23:48 +0200

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Queries the Reddit API
+"""
+import requests
+
+
+def subs(subreddit):
+    """
+        Returns the number of subscribers for a given
+        subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    UserAgent = {"User-Agent": "ubuntu:test:v1.0 (by /u/kemitche)"}
+
+    request = requests.get(url, headers=UserAgent)
+
+    if request.status_code == 404:
+        return 0
+
+    return request.json().get("data").get("subscribers")
