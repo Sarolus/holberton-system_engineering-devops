29c14906507baacaae4aa86a735f6bbe29562b42
Sarolus <cactussius.primus@gmail.com>
2021-10-05 12:15:05 +0200

    Add task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix syntax error
+
+exec {'replace':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
