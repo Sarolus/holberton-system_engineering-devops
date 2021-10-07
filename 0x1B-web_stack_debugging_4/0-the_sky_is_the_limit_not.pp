e7632efb68289e8db5627c79bb1e22ef45cf288e
Sarolus <cactussius.primus@gmail.com>
2021-10-07 16:20:36 +0200

    Optimize puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,22 @@
 # Increase Ulimit
 
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  name    => 'nginx',
+  require => Exec['update'],
+}
+
 file {'/etc/default/nginx':
+  ensure  => present,
   path    => '/etc/default/nginx',
   content => 'ULIMIT="-n 4096"',
 }
 
-exec {'nginx':
-  command => 'sudo service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Increase hard and soft file limit for user
 
 exec {'increase-hard-file':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton hard/s/5/1024/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin',
 }
 
 exec {'increase-soft-file':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton soft/s/4/1024/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin',
 }
