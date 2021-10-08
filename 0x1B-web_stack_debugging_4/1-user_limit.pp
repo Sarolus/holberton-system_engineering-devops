931b0e7ffc7033850f9e304cfe6bc59db8eca159
Sarolus <cactussius.primus@gmail.com>
2021-10-08 03:31:06 +0200

    Lower user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Increase hard and soft file limit for user
 
 exec {'increase-hard-file-limit':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/5000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin',
 }
 
 exec {'increase-soft-file-limit':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/5000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin',
 }
