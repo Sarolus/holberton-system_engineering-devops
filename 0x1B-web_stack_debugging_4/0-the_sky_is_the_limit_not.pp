97907d06b76a8ef3002ef27183974b5e6051eb3b
Sarolus <cactussius.primus@gmail.com>
2021-10-08 03:23:09 +0200

    Trying to fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,22 +1,12 @@
 # Increase Ulimit
 
-exec { 'update':
-  command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-  ensure  => installed,
-  name    => 'nginx',
-  require => Exec['update'],
-}
-
 file {'/etc/default/nginx':
   ensure  => present,
   path    => '/etc/default/nginx',
   content => 'ULIMIT="-n 4096"',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/bin', '/usr/bin'],
 }
